277848cfc72cd68dd93e7dfcc3f840147fd1670f
Hansanghyeon <999@hyeon.pro>
2022-04-27 21:37:52 +0900

    init docker

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  images: {
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/styles')],
+  },
+  experimental: {
+    outputStandalone: true,
+  },
 }
 
 module.exports = nextConfig
