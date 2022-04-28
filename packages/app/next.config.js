31f2c0d709bc4fd27561c8f95f67176c7f7720f6
Hansanghyeon <999@hyeon.pro>
2022-04-28 20:16:45 +0900

    feat: lerna 컨셉

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -10,6 +10,14 @@ const nextConfig = {
   experimental: {
     outputStandalone: true,
   },
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+
+    return config
+  },
 }
 
 module.exports = nextConfig
