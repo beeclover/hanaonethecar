76afed53eb3da15fc61abead71ae9cf2c0771758
Hansanghyeon <999@hyeon.pro>
2022-04-28 21:26:46 +0900

    feat: pretandard 폰트추가

---
+++ packages/app/pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="ko">
+      <Head>
+        <link rel="preload" href="/fonts/Pretendard-1.3.0/web/static/pretendard.css" as="font" crossOrigin="" />
+        <link rel="preload" href="/fonts/Pretendard-1.3.0/web/static/pretendard-subset.css" as="font" crossOrigin="" />
+        <link rel="preload" href="/fonts/Pretendard-1.3.0/web/variable/pretendardvariable.css" as="font" crossOrigin="" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
