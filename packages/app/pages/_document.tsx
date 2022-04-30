145a828b0ad6711d2297d98370e3412acebd7b8f
Hansanghyeon <999@hyeon.pro>
2022-04-30 11:24:37 +0900

    feat: hana 웹폰트 추가

--- packages/app/pages/_document.tsx
+++ packages/app/pages/_document.tsx
@@ -7,6 +7,7 @@ export default function Document() {
         <link rel="preload" href="/fonts/Pretendard-1.3.0/web/static/pretendard.css" as="font" crossOrigin="" />
         <link rel="preload" href="/fonts/Pretendard-1.3.0/web/static/pretendard-subset.css" as="font" crossOrigin="" />
         <link rel="preload" href="/fonts/Pretendard-1.3.0/web/variable/pretendardvariable.css" as="font" crossOrigin="" />
+        <link rel="preload" href="/fonts/hana/Hana.css" as="font" crossOrigin="" />
       </Head>
       <body>
         <Main />
