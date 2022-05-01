4cd6851b20c2b87f22cf330013167f844ee03bdf
Hansanghyeon <999@hyeon.pro>
2022-05-01 15:16:20 +0900

    feat: Auth0 로그인 완료

--- packages/app/pages/index.tsx
+++ packages/app/pages/index.tsx
@@ -15,7 +15,7 @@ const HomePage: NextPage = () => {
   `);
 
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+  if (error) return <p>Error :( {error.message}</p>;
 
   console.log(data);
 
