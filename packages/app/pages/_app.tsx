feca211bc06ee800c2c9e3a59660c0ef900a230d
Hansanghyeon <999@hyeon.pro>
2022-05-01 14:46:33 +0900

    feat: Auth0 SDK 추가

--- packages/app/pages/_app.tsx
+++ packages/app/pages/_app.tsx
@@ -2,13 +2,18 @@ import '~/styles/globals.scss'
 import '~/styles/tailwind.scss'
 import type { AppProps } from 'next/app'
 import { ApolloProvider } from '@apollo/client'
+import { UserProvider } from '@auth0/nextjs-auth0'
 import apolloClient from "~/lib/apollo";
 
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <ApolloProvider client={apolloClient}>
-    <Component {...pageProps} />
-  </ApolloProvider>
-}
+  return (
+    <UserProvider>
+      <ApolloProvider client={apolloClient}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </UserProvider>
+  );
+};
 
 export default MyApp
